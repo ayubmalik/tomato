1222e40b08f7695e6c1eb9277fd05d119e549530
Ayub Malik <ayub.malik@coopdigital.co.uk>
2020-01-02 15:08:30 +0000

    Add hide cursor

--- tomato.go
+++ tomato.go
@@ -15,6 +15,7 @@ func New(w io.Writer, d time.Duration) chan int {
 	go func() {
 		time.AfterFunc(d, func() {
 			ticker.Stop()
+			io.WriteString(w, "\033[?25h\033[0m")
 			close(quit)
 		})
 
@@ -33,6 +34,6 @@ func New(w io.Writer, d time.Duration) chan int {
 
 func write(w io.Writer, remaining int) {
 	d := time.Duration(remaining) * time.Second
-	s := fmt.Sprintf("\033[1K\r\033[38;5;226;7;1m%s\033[0m", d)
+	s := fmt.Sprintf("\033[?25l\033[1K\r\033[38;5;226;7;1m%s\033[0m", d)
 	io.WriteString(w, s)
 }
